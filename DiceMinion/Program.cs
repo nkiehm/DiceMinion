136ee7e02c1ec1ac853042ee6c52354fa4c859d5
nkiehm <nathanielkiehm@gmail.com>
2025-03-01 21:27:38 -0500

    initial commit established initial discord requests

---
+++ DiceMinion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .AddJsonFile("discord_settings.json")
+    .Build();
+
+var disc_url = config["disc_url"];
+var bot_token = config["bot_token"];
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri(disc_url);
+client.DefaultRequestHeaders.Add("User-Agent", "DiscordBot (https://localhost, 0.1.0)");
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bot", bot_token);
+
+var response = await client.GetAsync("gateway/bot");
+var responseBody = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(response);
+Console.WriteLine(responseBody);
